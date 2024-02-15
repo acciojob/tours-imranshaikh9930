e6dea7dab0eba963d7d3350f149e80404902eac0
Imran shaikh <imranpathan7040@gmail.com>
2024-02-15 14:00:29 +0530

    more btn id added

--- src/components/Tour.js
+++ src/components/Tour.js
@@ -11,7 +11,7 @@ function Tour({tour,removeTour}) {
         <img src={tour.image} alt={tour.name} />
         <div>
             <h3>{tour.name}</h3>
-            <p id={`tour-item-para-${tour.id}`}>{showMore ? tour.info : `${tour.info.substring(0, 200)}...`} <button onClick={toggleShowMore}>{showMore ? 'See less' : 'Show more'}</button></p>
+            <p id={`tour-item-para-${tour.id}`}>{showMore ? tour.info : `${tour.info.substring(0, 200)}...`} <button onClick={toggleShowMore} id={`see-more-{tour.id}`}>{showMore ? 'See less' : 'Show more'}</button></p>
             <p>{tour.price}</p>
             <button id={`delete-btn-${tour.id}`} onClick={()=>removeTour(tour.id)}>Delete</button>
         </div>
