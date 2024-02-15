87a95d32a60d6f23c9d41a6b3d0c2e4f8853dfbb
Imran shaikh <imranpathan7040@gmail.com>
2024-02-15 13:51:53 +0530

    btnadded

--- src/components/Tour.js
+++ src/components/Tour.js
@@ -11,9 +11,9 @@ function Tour({tour,removeTour}) {
         <img src={tour.image} alt={tour.name} />
         <div>
             <h3>{tour.name}</h3>
-            <p>{showMore ? tour.info : `${tour.info.substring(0, 200)}...`} <button onClick={toggleShowMore}>{showMore ? 'See less' : 'Show more'}</button></p>
+            <p id="#tour-item-para">{showMore ? tour.info : `${tour.info.substring(0, 200)}...`} <button onClick={toggleShowMore}>{showMore ? 'See less' : 'Show more'}</button></p>
             <p>{tour.price}</p>
-            <button onClick={()=>removeTour(tour.id)}>Delete</button>
+            <button id="delete-btn" onClick={()=>removeTour(tour.id)}>Delete</button>
         </div>
     </article>
   )
